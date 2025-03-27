366e5bcecb7eef8fde267d4b895c4bc04e8c9230
ravitejamannam <ravitejamannam88@gmail.com>
2025-03-27 21:05:48 +0530

    Added functions to split text nodes into links and images

---
+++ src/split_nodes_link_image.py
@@ -0,0 +1,53 @@
+import re
+from textnode import TextNode, TextType
+from extract_links import extract_markdown_links, extract_markdown_images
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for alt_text, url in images:
+            parts = text.split(f"![{alt_text}]({url})", 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+            text = parts[1] if len(parts) > 1 else ""
+
+        if text:
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for anchor_text, url in links:
+            parts = text.split(f"[{anchor_text}]({url})", 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(anchor_text, TextType.LINK, url))
+            text = parts[1] if len(parts) > 1 else ""
+
+        if text:
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
